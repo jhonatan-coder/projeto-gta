512df5b5ae2f5f116f5b896248e009c2201ba718
jhonatan-coder <55715032+jhonatan-coder@users.noreply.github.com>
2024-06-14 13:00:17 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,58 @@
 /*
+    OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
+        = passo 1 - pegar o elemento HTML da seta avancar
+        - passo 2 - identificar o clique do usuário na seta avançar
+            - passo 3 - fazer aparecer o proximo cartão da lista
+            - passo 4 - buscar o cartão que está selecionado e esconder
 
-Objetivo 1 - quando o usuário clicar no botão de seleção de plataformas deve abrir uma caixa com os botões de seleção de plataforma
-    Passo 1 - pegar o botão de seleção de plataformas no JS pra poder verificar quando o usuário clicar em cima dele
-    
-    Passo 2 - pegar o elemento do conteudo que precisa ser mostrado
-
-    Passo 3 - pegar o clique do usuario no js
-
-    Passo 4 - quando o usuário clicar, adicionar a classe ativo na listagem de plataformas dentro do botão pra que o conteúdo apareça
-
-
-Objetivo 2 - caso a lista de botões de plataformas já esteja aparecendo e o usuário clicar em cima do botão, o conteúdo deve ser escondido
-
-    Passo 1 - verificar se o botão já esta aberto, se sim, devemos remover a classe ativo pra que ele esconda o conteúdo novamente
-
+    OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
+        - passo 1 - pegar o elemento HTML da seta voltar
+        - passo 2 - identificar o clique do usuário na seta voltar
+        - passo 3 - fazer aparecer o cartão anterior da lista
+            - passo 4 -  buscar o cartão que esta selecionado e esconder
 */
 
-const botao = document.querySelector(".btn-plataforma");
-const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+// OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo cartao da lista
+// = passo 1 - pegar o elemento HTML da seta avancar
+const btnAvancar = document.getElementById("btn-avancar");
+// - passo 1 - pegar o elemento HTML da seta voltar
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
 
-botao.addEventListener("click", () => {
-    elementoPlataformas.classList.toggle("ativo");
-});
+// - passo 2 - identificar o clique do usuário na seta avançar
+btnAvancar.addEventListener("click", function () {
+    const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+    if (ehUltimoCartao) return;
 
+    // - passo 4 - buscar o cartão que está selecionado e esconder
+    esconderCartaoSelecionado();
+
+    // - passo 3 - fazer aparecer o proximo cartão da lista, colocando a classe selecionado nele
+    cartaoAtual++;
+
+    mostrarCartao();
+})
+//OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
+// - passo 2 - identificar o clique do usuário na seta voltar
+btnVoltar.addEventListener("click", function () {
+    const ehPrimeiroCartao = cartaoAtual === 0;
+    if(ehPrimeiroCartao) return;
+     // - passo 4 - buscar o cartão que está selecionado e esconder
+     esconderCartaoSelecionado();
+     // - passo 3 - fazer aparecer o proximo cartão da lista, colocando a classe selecionado nele
+     cartaoAtual--;
+     mostrarCartao();
+})
+
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+
+    cartaoSelecionado.classList.remove("selecionado");
+}
 
